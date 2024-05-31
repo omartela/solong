f6794d5407b6c369e7b2c3529d7246a0f6446ef0
Oskari Martela <oskari.martela@gmail.com>
2024-05-31 15:37:54 +0300

    Adds header file for project

---
+++ so_long.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 14:52:19 by omartela          #+#    #+#             */
+/*   Updated: 2024/05/31 14:56:33 by omartela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "MLX42/include/MLX42/MLX42.h"
+
+#endif
