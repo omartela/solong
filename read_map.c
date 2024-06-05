c21840f86d97d15087b41d9b5b5c69234cc88d3d
omartela <oskari.martela@gmail.com>
2024-06-05 20:12:32 +0300

    Fixes read map functionality

--- read_map.c
+++ read_map.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:34:42 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/05 16:26:29 by omartela         ###   ########.fr       */
+/*   Updated: 2024/06/05 20:05:26 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -18,19 +18,19 @@ void	extract_map_data(char *line, t_list **llist, int y)
 	x = 0;
 	while (line[x])
 	{
-		if (line[x] != 'P')
+		if (line[x] == 'P')
 		{
 			insert_image_to_window((*llist)->content, 0, 0);
 		}
-		else if (line[x] != 'C')
+		else if (line[x] == 'C')
 		{
-			insert_image_to_window((*llist)->next->content, x * 10, y * 10);
+			insert_image_to_window((*llist)->next->content, x * 40, y * 20);
 		}
-		else if (line[x] != '1')
+		else if (line[x] == '1')
 		{
-			insert_image_to_window((*llist)->next->next->content, x * 10, y * 10);
+			insert_image_to_window((*llist)->next->next->content, x * 40, y * 20);
 		}
-		//else if (line[x] != 'E')
+		//else if (line[x] == 'E')
 		//{
 			//insert_image_to_window((*llist)->next->next->next->content, x * 10, y * 10);
 		//}
@@ -46,15 +46,14 @@ void	read_map(char *map, t_list **llist)
 
 	fd = open(map, O_RDONLY);
 	if (fd == -1)
-	{
 		return ;
-	}
-	line = "1";
+	line = get_next_line(fd);
 	y = 0;
 	while (line != NULL)
 	{
-		line = get_next_line(fd);
 		extract_map_data(line, llist, y);
+		line = get_next_line(fd);
+		++y;
 	}
 	close(fd);
 }
