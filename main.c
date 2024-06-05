6907c6885085655f2a0bea0592307796b137bf77
omartela <oskari.martela@gmail.com>
2024-06-05 20:57:48 +0300

    Adds collide function

--- main.c
+++ main.c
@@ -6,41 +6,42 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 12:46:18 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/05 16:27:21 by omartela         ###   ########.fr       */
+/*   Updated: 2024/06/05 20:56:55 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "so_long.h"
 
-/*
-int	check_collision(mlx_image_t *img1, mlx_image_t *img2)
+int check_collision(mlx_image_t *img1, mlx_image_t *img2)
 {
-	if (img1->instances[0].x < img2->instances[0].x + img2->width
-			&& img1->instances[0].x + img1->width > img2->instances[0].x)
-		return (1);
-	if (img1->instances[0].y < img2->instances[0].y + img2->height
-			&& img1->instances[0].y + img1->height > img2->instances[0].y)
-		return (1);
-	return (0);
+    if ((unsigned int)img1->instances[0].x < (unsigned int)img2->instances[0].x + img2->width &&
+        (unsigned int)(img1->instances[0].x + img1->width) > (unsigned int)img2->instances[0].x &&
+        (unsigned int)img1->instances[0].y < (unsigned int)img2->instances[0].y + img2->height &&
+        (unsigned int)(img1->instances[0].y + img1->height) > (unsigned int)img2->instances[0].y)
+    {
+        return 1;
+    }
+    return 0;
 }
-*/
+
 void	ft_hook(void *param)
 {
 	mlx_t	*mlx;
 	t_img	*params;
+	t_list	*llist;
 
-	params = (t_img *)param;
+	llist = (t_list *)param;
+	params = (t_img *)llist->content;
 	mlx = params->mlx;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		params->image->instances[0].y -= 5;
+		params->image->instances[0].y -= 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		params->image->instances[0].y += 5;
+		params->image->instances[0].y += 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		params->image->instances[0].x -= 5;
+		params->image->instances[0].x -= 1;
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		params->image->instances[0].x += 5;
+		params->image->instances[0].x += 1;
 }
 
 void	insert_image_to_window(void *content, int x, int y)
@@ -63,21 +64,17 @@ int	main(void)
 	t_list		*llist;
 
 	llist = NULL;
-	mlx = mlx_init(500, 500, "Test", true);
+	mlx = mlx_init(500, 500, "Dwarf & Diamonds", true);
 	if (!mlx)
 		error();
 	load_images_to_struct(&llist, "DwarfSprite1.png", mlx);
 	load_images_to_struct(&llist, "golddiamond.png", mlx);
-	load_images_to_struct(&llist, "Rock1.png", mlx);
+	load_images_to_struct(&llist, "Rock Pile 1 - AZURE - small.PNG", mlx);
 	//insert_image_to_window(llist->next->next->content);
 	//set_image_position(llist->next->next->content, 100, 100);
 	//ft_lstiter(llist, &insert_image_to_window);
-	insert_image_to_window(llist->content, 0, 0);
-	insert_image_to_window(llist->next->content, 50, 50);
-	insert_image_to_window(llist->next->next->content, 100, 100);
-	insert_image_to_window(llist->next->next->content, 150, 150);
-	//read_map("testmap.ber", &llist);
-	mlx_loop_hook(mlx, ft_hook, llist->content);
+	read_map("testmap.ber", &llist);
+	mlx_loop_hook(mlx, ft_hook, llist);
 	mlx_loop(mlx);
 	//mlx_delete_image(mlx, i_s1.image);
 	//mlx_delete_image(mlx, i_s2.image);
