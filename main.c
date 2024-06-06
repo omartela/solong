d333e4c1f8e3737dcc50bbd8df727b8436012715
Oskari Martela <oskari.martela@gmail.com>
2024-06-06 15:38:54 +0300

    Fixes collision checking and add argc and argv

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-??? from here until ???END lines may have been inserted/deleted
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,81 +5,103 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/31 12:46:18 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/05 23:17:41 by omartela         ###   ########.fr       */
+/*   Created: 2024/06/06 10:05:25 by omartela          #+#    #+#             */
+/*   Updated: 2024/06/06 13:07:08 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
-
-int check_collision(mlx_image_t *img1, mlx_image_t *img2, int i)
+#include <stdio.h>
+int	check_collision(mlx_image_t *obstacle, mlx_image_t *p, int i)
 {
-    if ((unsigned int)img1->instances[i].x < (unsigned int)img2->instances[i].x + img2->width &&
-        (unsigned int)img1->instances[i].x + img1->width > (unsigned int)img2->instances[i].x &&
-        (unsigned int)img1->instances[i].y < (unsigned int)img2->instances[i].y + img2->height &&
-        (unsigned int)(img1->instances[i].y + img1->height) > (unsigned int)img2->instances[i].y)
+	unsigned int	x1;
+	unsigned int	y1;
+	unsigned int	x2;
+	unsigned int	y2;
+
+	x1 = (unsigned int)obstacle->instances[i].x;
+	x2 = (unsigned int)p->instances[0].x;
+	y1 = (unsigned int)obstacle->instances[i].y;
+	y2 = (unsigned int)p->instances[0].y;
+	if (x1 < x2 + p->width && x1 + obstacle->width > x2 && y1 < y2 + p->height &&
+		y1 + obstacle->height > y2)
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
 
-int check_movement_collision_y(mlx_image_t *img1, mlx_image_t *img2, int i, int movement)
+int	check_movement_collision_y(mlx_image_t *obstacle, mlx_image_t *p, int i, int movement)
 {
-	int new_y;
+	unsigned int	x1;
+	unsigned int	x2;
+	unsigned int	y1;
+	unsigned int	y2;
 
-    new_y = img1->instances[i].y - movement;
-    if ((unsigned int)img1->instances[i].x < (unsigned int)img2->instances[i].x + img2->width &&
-        (unsigned int)(img1->instances[i].x + img1->width) > (unsigned int)img2->instances[i].x &&
-        (unsigned int)new_y < (unsigned int)img2->instances[i].y + img2->height &&
-        (unsigned int)new_y + img1->height > (unsigned int)img2->instances[i].y)
+	x1 = (unsigned int)obstacle->instances[i].x;
+	x2 = (unsigned int)p->instances[0].x;
+	y1 = (unsigned int)obstacle->instances[i].y;
+	y2 = (unsigned int)p->instances[0].y + movement;
+	if (x1 < x2 + p->width && x1 + obstacle->width > x2 && y1 < y2 + p->height &&
+		y1 + obstacle->height > y2)
 	{
-        return 1;
+		return (1);
 	}
-
-    return 0;
+	return (0);
 }
 
-
-int check_movement_collision_x(mlx_image_t *img1, mlx_image_t *img2, int i, int movement)
+int	check_movement_collision_x(mlx_image_t *obstacle, mlx_image_t *p, int i, int movement)
 {
-	int new_x;
+	unsigned int	x1;
+	unsigned int	x2;
+	unsigned int	y1;
+	unsigned int	y2;
 
-    new_x = img1->instances[i].x - movement;
-    if ((unsigned int)new_x < (unsigned int)img2->instances[i].x + img2->width &&
-        (unsigned int)new_x + img1->width > (unsigned int)img2->instances[i].x &&
-        (unsigned int)img1->instances[i].y < (unsigned int)img2->instances[i].y + img2->height &&
-        (unsigned int)(img1->instances[i].y + img1->height) > (unsigned int)img2->instances[i].y)
-    {
-        return 1;
-    }
-    return 0;
+	x1 = (unsigned int)obstacle->instances[i].x;
+	x2 = (unsigned int)p->instances[0].x + movement;
+	y1 = (unsigned int)obstacle->instances[i].y;
+	y2 = (unsigned int)p->instances[0].y;
+	if (x1 < x2 + p->width && x1 + obstacle->width > x2 && y1 < y2 + p->height &&
+		y1 + obstacle->height > y2)
+	{
+		return (1);
+	}
+	return (0);
 }
 
-
-int check_obstacle(void *obstacle, t_img *p, int movement)
+int	check_obstacle(void *obc, t_img *p, int movement, char direction)
 {
 	t_img	*obstacle;
-	int		i;
+	size_t	i;
 
-	obstacle = (t_img *)obstacle;
-	while (i < obstacle->image.count)
+	obstacle = (t_img *)obc;
+	i = 0;
+	while (i < obstacle->image->count)
 	{
-		if (check_movement_collision_x(obstacle, p, i, movement))
-			return (1);
+		if (direction == 'x')
+			if (check_movement_collision_x(obstacle->image, p->image, i, movement))
+				return (1);
+		if (direction == 'y')
+			if (check_movement_collision_y(obstacle->image, p->image, i, movement))
+				return (1);
+		++i;
 	}
+	return (0);
 }
 
-int check_collectable(void *content, t_img *p)
+int	check_collectable(void *content, t_img *p)
 {
 	t_img	*collectable;
-	int		i;
+	size_t	i;
 
 	i = 0;
 	collectable = (t_img *)content;
-	while (i < collectable->image.count)
+	while (i < collectable->image->count)
 	{
-		if (check_collision(collectable, p, i))
+		if (check_collision(collectable->image, p->image, i))
+		{
+			collectable->image->instances[i].enabled = false;
 			return (1);
+		}
 		++i;
 	}
 	return (0);
@@ -99,7 +120,7 @@ void	ft_hook(void *param)
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
 	{
-		if (check_obstacle(llist->next->next->content, 1))
+		if (!check_obstacle(llist->next->next->content, params, -1, 'y'))
 		{
 			params->image->instances[0].y -= 1;
 			check_collectable(llist->next->content, params);
@@ -107,16 +128,35 @@ void	ft_hook(void *param)
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
 	{
-		if (check_obstacle(llist->next->next->content, 1)
+		if (!check_obstacle(llist->next->next->content, params, 1, 'y'))
 		{
 			params->image->instances[0].y += 1;
 			check_collectable(llist->next->content, params);
 		}
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		params->image->instances[0].x -= 1;
+	{
+		if (!check_obstacle(llist->next->next->content, params, -1, 'x'))
+		{
+			params->image->instances[0].x -= 1;
+			check_collectable(llist->next->content, params);
+		}
+	}
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		params->image->instances[0].x += 1;
+	{
+		if (!check_obstacle(llist->next->next->content, params, 1, 'x'))
+		{
+			params->image->instances[0].x += 1;
+			check_collectable(llist->next->content, params);
+		}
+	}
+}
+void	resize_image(void *content, unsigned int x, unsigned int y)
+{
+	t_img	*img;
+
+	img = (t_img *)content;
+	mlx_resize_image(img->image, x, y);
 }
 
 void	insert_image_to_window(void *content, int x, int y)
@@ -133,22 +173,27 @@ void	set_image_position(t_img *img, int pos_x, int pos_y)
 	img->image->instances[1].y = pos_y;
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	mlx_t		*mlx;
 	t_list		*llist;
+	//char		**game;
 
 	llist = NULL;
+	argc = 2;
 	mlx = mlx_init(500, 500, "Dwarf & Diamonds", true);
 	if (!mlx)
 		error();
 	load_images_to_struct(&llist, "DwarfSprite1.png", mlx);
-	load_images_to_struct(&llist, "golddiamond.png", mlx);
+	load_images_to_struct(&llist, "Gems A 1.png", mlx);
 	load_images_to_struct(&llist, "Rock Pile 1 - AZURE - small.PNG", mlx);
+	resize_image(llist->content, 40, 40);
+	resize_image(llist->next->content, 40, 40);
+	resize_image(llist->next->next->content, 40, 40);
 	//insert_image_to_window(llist->next->next->content);
 	//set_image_position(llist->next->next->content, 100, 100);
 	//ft_lstiter(llist, &insert_image_to_window);
-	read_map("testmap.ber", &llist);
+	read_map(argv[1], &llist);
 	mlx_loop_hook(mlx, ft_hook, llist);
 	mlx_loop(mlx);
 	//mlx_delete_image(mlx, i_s1.image);
