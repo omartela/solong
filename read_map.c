f3d49db49b151ba58e92ab08c4a34d32aad45093
Oskari Martela <oskari.martela@gmail.com>
2024-06-07 18:00:46 +0300

    Implements map reading to 2d array, implements own ft_realloc

--- read_map.c
+++ read_map.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 14:34:42 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/07 11:45:00 by omartela         ###   ########.fr       */
+/*   Updated: 2024/06/07 17:59:36 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -14,7 +14,7 @@
 // error handling for the map
 // implement check that in the map there is no symbols which are not allowed.
 // implement check that player cant reach collectable or door
-//
+// 
 
 void	extract_map_data(char *line, t_list **llist, int y)
 {
@@ -47,22 +47,79 @@ void	extract_map_data(char *line, t_list **llist, int y)
 	}
 }
 
+int	validate_map_char(char c)
+{
+	if (ft_strchr("PC1E0", c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
+{
+	void	*new_ptr;
+	size_t	copy_size;
+
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	if (old_size < new_size)
+	{
+		copy_size = old_size;
+	}
+	else
+	{
+		copy_size = new_size;
+	}
+	if (ptr)
+	{
+		ft_memcpy(new_ptr, ptr, copy_size);
+		free(ptr);
+	}
+	return (new_ptr);
+}
+
 void	read_map(char *file, t_list **llist)
 {
 	int		fd;
+	char	**map;
+	int		lines_read;
 	char	*line;
-	int		y;
 
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return ;
 	line = get_next_line(fd);
-	y = 0;
+	if (!line)
+	{
+		close(fd);
+		return ;
+	}
+	map = malloc(1 * sizeof(char *));
+	if (!map)
+	{
+		close(fd);
+		return ;
+	}
+	map[0] = line;
+	lines_read = 1;
 	while (line != NULL)
 	{
-		extract_map_data(line, llist, y);
+		extract_map_data(line, llist, lines_read - 1);
 		line = get_next_line(fd);
-		++y;
+		map = ft_realloc(map, lines_read * sizeof(char *), (lines_read + 1) * sizeof(char *));
+		if (!map)
+		{
+			free(line);
+			close(fd);
+			return ;
+		}
+		map[lines_read] = line;
+		++lines_read;
 	}
 	close(fd);
 }
