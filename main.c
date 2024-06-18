a7a51148e97c70432cfcf0b2b93f3c9ab2544075
omartela <oskari.martela@gmail.com>
2024-06-18 21:50:00 +0300

    Adds png\ to main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 10:05:25 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/17 11:38:16 by omartela         ###   ########.fr       */
+/*   Updated: 2024/06/18 21:37:12 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -19,34 +19,34 @@ void	init_game_images(t_game *game, t_list **llist)
 	mlx_put_string(game->mlx, "Score:", 17 * 10, 0);
 	game->move_count_image = mlx_put_string(game->mlx, "0", 12 * 10, 0);
 	game->score_image = mlx_put_string(game->mlx, "0", 24 * 10, 0);
-	load_image_to_struct(llist, "DwarfSprite1.png", game->mlx);
+	load_image_to_struct(llist, "png/DwarfSprite1.png", game->mlx);
 	img = (t_img *)(*llist)->content;
-	img->right_images[0] = "dwalk1.png";
-	img->right_images[1] = "dwalk2.png";
-	img->right_images[2] = "dwalk3.png";
-	img->right_images[3] = "dwalk4.png";
-	img->right_images[4] = "dwalk5.png";
-	img->right_images[5] = "dwalk6.png";
-	img->right_images[6] = "dwalk7.png";
-	img->right_images[7] = "dwalk8.png";
-	img->left_images[0] = "dwalk1_left.png";
-	img->left_images[1] = "dwalk2_left.png";
-	img->left_images[2] = "dwalk3_left.png";
-	img->left_images[3] = "dwalk4_left.png";
-	img->left_images[4] = "dwalk5_left.png";
-	img->left_images[5] = "dwalk6_left.png";
-	img->left_images[6] = "dwalk7_left.png";
-	img->left_images[7] = "dwalk8_left.png";
-	img->r_idle_images[0] = "DwarfSprite1.png";
-	img->l_idle_images[0] = "DwarfSprite_left.png";
+	img->right_images[0] = "png/dwalk1.png";
+	img->right_images[1] = "png/dwalk2.png";
+	img->right_images[2] = "png/dwalk3.png";
+	img->right_images[3] = "png/dwalk4.png";
+	img->right_images[4] = "png/dwalk5.png";
+	img->right_images[5] = "png/dwalk6.png";
+	img->right_images[6] = "png/dwalk7.png";
+	img->right_images[7] = "png/dwalk8.png";
+	img->left_images[0] = "png/dwalk1_left.png";
+	img->left_images[1] = "png/dwalk2_left.png";
+	img->left_images[2] = "png/dwalk3_left.png";
+	img->left_images[3] = "png/dwalk4_left.png";
+	img->left_images[4] = "png/dwalk5_left.png";
+	img->left_images[5] = "png/dwalk6_left.png";
+	img->left_images[6] = "png/dwalk7_left.png";
+	img->left_images[7] = "png/dwalk8_left.png";
+	img->r_idle_images[0] = "png/DwarfSprite1.png";
+	img->l_idle_images[0] = "png/DwarfSprite_left.png";
 	img->previous_dir = 'r';
 	img->ri = 0;
 	img->i_ri = 0;
 	img->i_li = 0;
 	img->li = 0;
-	load_image_to_struct(llist, "amethyst.png", game->mlx);
-	load_image_to_struct(llist, "Rock Pile 1 - AZURE - small.PNG", game->mlx);
-	load_image_to_struct(llist, "Door02.png", game->mlx);
+	load_image_to_struct(llist, "png/amethyst.png", game->mlx);
+	load_image_to_struct(llist, "png/Rock Pile 1 - AZURE - small.PNG", game->mlx);
+	load_image_to_struct(llist, "png/Door02.png", game->mlx);
 	resize_image((*llist)->content, TILE_SIZE, TILE_SIZE);
 	resize_image((*llist)->next->content, TILE_SIZE, TILE_SIZE);
 	resize_image((*llist)->next->next->content, TILE_SIZE, TILE_SIZE);
