0dfbf90bde27ab8cf799982f8598b82d375b24b2
Oskari Martela <omartela@c1r5p13.hive.fi>
2024-06-28 16:06:36 +0300

    Frees the memory allocated by ft_itoa

--- movement.c
+++ movement.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:25:10 by omartela          #+#    #+#             */
-/*   Updated: 2024/06/28 12:19:18 by omartela         ###   ########.fr       */
+/*   Updated: 2024/06/28 16:02:14 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "so_long.h"
@@ -30,14 +30,19 @@ void	move_up(t_game *game)
 void	print_moves_and_score(t_game *game)
 {
 	mlx_image_t	*img;
+	char		*str;
 
 	if (game->move_count_image)
 		mlx_delete_image(game->mlx, game->move_count_image);
-	img = mlx_put_string(game->mlx, ft_itoa(game->move_count), 12 * 10, 0);
+	str = ft_itoa(game->move_count);
+	img = mlx_put_string(game->mlx, str, 12 * 10, 0);
+	free(str);
 	game->move_count_image = img;
 	if (game->score_image)
 		mlx_delete_image(game->mlx, game->score_image);
-	img = mlx_put_string(game->mlx, ft_itoa(game->score), 25 * 10, 0);
+	str = ft_itoa(game->score);
+	img = mlx_put_string(game->mlx, str, 25 * 10, 0);
+	free(str);
 	game->score_image = img;
 }
 
@@ -95,13 +100,13 @@ void	ft_hook_movement(mlx_key_data_t keydata, void *param)
 {
 	t_img	*player;
 	t_game	*game;
-	t_list	*llist;
 	t_img	*enemy;
+	int		flag;
 
+	flag = 0;
 	game = (t_game *)param;
-	llist = game->llist;
-	player = (t_img *)llist->content;
-	enemy = llist->next->next->next->next->content;
+	player = (t_img *)game->llist->content;
+	enemy = game->llist->next->next->next->next->content;
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
 		move_up(game);
 	if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
@@ -113,9 +118,14 @@ void	ft_hook_movement(mlx_key_data_t keydata, void *param)
 	if (keydata.action == MLX_PRESS)
 		move_enemy(game);
 	if (check_collision_to_player(enemy, player))
+	{
+		flag = 1;
 		player->image->instances[0].enabled = false;
+	}
 	print_moves_and_score(game);
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-		exit_game(game);
-	check_exit(game, player);
+		flag = 1;
+	if (check_exit(game, player))
+		flag = 1;
+	exit_game(game, flag);
 }
